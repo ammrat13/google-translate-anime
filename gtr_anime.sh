6254eb64c179a30669e836693c996308e8480446
ammrat13 <ammrat13@gmail.com>
2018-03-14 18:38:18 -0500

    Bugfixes and a new example

--- gtr_anime.sh
+++ gtr_anime.sh
@@ -33,7 +33,7 @@ function download_sub_url () {
 	# Check each possible site
 	# Crunchyroll
 	if [[ $URL =~ http[s]?:\/\/www.crunchyroll\.com[^\s]* ]]; then
-		youtube-dl -o "$ODIR/old.enUS.srt" --write-sub --sub-format srt \
+		youtube-dl -o "$ODIR/old.srt" --write-sub --sub-format srt \
 			--sub-lang enUS --skip-download $URL
 		mv $ODIR/old.enUS.srt $ODIR/old.srt
 	fi
@@ -50,7 +50,7 @@ fi
 
 
 # Only try to obtain the video file if it is not already present
-if [[ ! -f "$ODIR/old.mp4" ]]; then
+if [[ ! -f "$ODIR/old.mp4" ]] || [[ ! -s "$ODIR/old.mp4" ]]; then
 	# If the video file exists and is mp4, copy it as is
 	if [[ -f $VIDLOC ]] && [[ $VIDLOC =~ .*\.mp4 ]]; then
 		echo "The file $VIDLOC is already mp4. Copying to $ODIR/old.mp4 ..."
@@ -58,15 +58,15 @@ if [[ ! -f "$ODIR/old.mp4" ]]; then
 	# If the file exists but is not an mp4, then convert with ffmpeg
 	elif [[ -f $VIDLOC ]]; then
 		echo "The file $VIDLOC exists but is not mp4. Converting and outputing to $ODIR/old.mp4 ..."
-		ffmpeg -i $VIDLOC "$ODIR/old.mp4" &> /dev/null
+		ffmpeg -y -i $VIDLOC "$ODIR/old.mp4" &> /dev/null
 	# If the location is a valid url, download it
 	elif [[ $VIDLOC =~ http[^\s]* ]]; then
-		echo "The string $VIDLOC is a url. Downloading video ..."
+		echo "The string $VIDLOC is a url. Downloading video (this may take a while) ..."
 		download_vid_url $VIDLOC &> /dev/null
 	fi
 
 	# Make sure the file exists now
-	if [[ ! -f "$ODIR/old.mp4" ]]; then
+	if [[ ! -f "$ODIR/old.mp4" ]] || [[ ! -s "$ODIR/old.mp4" ]]; then
 		echo "Failed to obtain video file. Terminating."
 		exit
 	fi
@@ -78,7 +78,7 @@ echo ""
 
 
 # Only try to obtain the subtitles file if it is not already present
-if [[ ! -f "$ODIR/old.srt" ]]; then
+if [[ ! -f "$ODIR/old.srt" ]] || [[ ! -s "$ODIR/old.srt" ]]; then
 	# If the subtitles file exists and is enUS-srt, copy it as is
 	if [[ -f $SUBLOC ]] && [[ $SUBLOC =~ .*\.[a-zA-Z]+\.srt ]]; then
 		echo "The file $SUBLOC is already srt. Copying to $ODIR/old.srt ..."
@@ -90,7 +90,7 @@ if [[ ! -f "$ODIR/old.srt" ]]; then
 	fi
 
 	# Make sure the file exists now
-	if [[ ! -f "$ODIR/old.srt" ]]; then
+	if [[ ! -f "$ODIR/old.srt" ]] || [[ ! -s "$ODIR/old.srt" ]]; then
 		echo "Failed to obtain subtitles file. Terminating."
 		exit
 	fi
@@ -104,21 +104,20 @@ echo ""
 # Get the times for each clip
 vid_times=($(./get_times.py "$ODIR/old.srt"))
 
-# Only translate if we have not already
-if [[ ! -f "$ODIR/tmp/raw_subs.txt" ]] || [[ $(wc -l < $ODIR/tmp/raw_subs.txt) -ne ${#vid_times[@]} ]]; then
-	echo "Splitting video into wav files and translating ..."
-	# Create output file if it does not exist and clear it if it does
-	> $ODIR/tmp/raw_subs.txt
+echo "Splitting video into flac files and translating ..."
+for line in ${vid_times[@]}; do
+	# Split on commas into the array ts
+	IFS="," ts=($line)
+	IFS=$' \t\n'
 
-	for line in ${vid_times[@]}; do
-		# Split on commas into the array ts
-		IFS="," ts=(${line})
+	# Only translate if we have not already
+	if [[ ! -f "$ODIR/tmp/${ts[0]}.sub.txt" ]] || [[ ! -s "$ODIR/tmp/${ts[0]}.sub.txt" ]]; then
 		echo "Processing clip from ${ts[0]} to ${ts[1]}"
 
 		# Output the audio between those times
-		ffmpeg -i "$ODIR/old.mp4" -ss ${ts[0]} -t $(echo "${ts[1]}-${ts[0]}" | bc) \
+		ffmpeg -y -i "$ODIR/old.mp4" -ss ${ts[0]} -t $(echo "${ts[1]}-${ts[0]}" | bc | awk '{printf "%.2f\n", $0}') \
 			-ar 44100 -ac 1 "$ODIR/tmp/${ts[0]}.flac" &> /dev/null
-		
+
 		# Google Speech API as specified to save the result to jap
 		jap=$(
 			echo "{
@@ -141,8 +140,6 @@ if [[ ! -f "$ODIR/tmp/raw_subs.txt" ]] || [[ $(wc -l < $ODIR/tmp/raw_subs.txt) -
 			jq -r .results[0].alternatives[0].transcript
 		)
 
-		echo "Japanese transcript: $jap"
-
 		# Google Translate API as specified to save the result to eng
 		eng=$(
 			curl -s -X POST \
@@ -156,50 +153,61 @@ if [[ ! -f "$ODIR/tmp/raw_subs.txt" ]] || [[ $(wc -l < $ODIR/tmp/raw_subs.txt) -
 			}" "https://translation.googleapis.com/language/translate/v2" | \
 			jq -r .data.translations[0].translatedText
 		)
-		echo "English translation: $eng"
 
 		# Append to output file
-		echo $eng >> $ODIR/tmp/raw_subs.txt
-	done
-else
-	echo "The video has already been translated. No need to redo."
-fi
-# Make sure translation was successful
-if [[ ! -f "$ODIR/tmp/raw_subs.txt" ]] || [[ $(wc -l < $ODIR/tmp/raw_subs.txt) -ne ${#vid_times[@]} ]]; then
-	echo "Translation failed. Terminating."
-	exit
-fi
+		echo $eng > $ODIR/tmp/${ts[0]}.sub.txt
+	fi
+
+	# Make sure translation succeeded
+	if [[ ! -f "$ODIR/tmp/${ts[0]}.sub.txt" ]] || [[ ! -s "$ODIR/tmp/${ts[0]}.sub.txt" ]]; then
+		echo "Failed to translate. Terminating."
+		exit
+	fi
+done
 
 echo ""
 
 
 # Generate new subtitles
 echo "Generating new subtitles ..."
+# Consolidate subtitles
+> $ODIR/tmp/raw_subs.txt
+for line in ${vid_times[@]}; do
+	# Split on commas into the array ts
+	IFS="," ts=($line)
+	IFS=$' \t\n'
+	cat "$ODIR/tmp/${ts[0]}.sub.txt" >> $ODIR/tmp/raw_subs.txt
+done
 ./new_subs.py $ODIR/old.srt $ODIR/tmp/raw_subs.txt > $ODIR/new.srt
+
+echo ""
+
 
 # Generate new voices if we have not already
 echo "Generating voiceovers ..."
 IFS=$(echo -en "\n\b") raw_subs=($(cat "$ODIR/tmp/raw_subs.txt"))
+IFS=$' \t\n'
 i=0
 for line in ${vid_times[@]}; do
 	# Split on commas into the array ts
 	IFS="," ts=(${line})
+	IFS=$' \t\n'
 
 	# Only generate new voices if we have not already
-	if [[ ! -f "$ODIR/tmp/${ts[0]}.trans.m4a" ]]; then
+	if [[ ! -f "$ODIR/tmp/${ts[0]}.trans.m4a" ]] || [[ ! -s "$ODIR/tmp/${ts[0]}.trans.m4a" ]]; then
 		# Make sure subtitles were generated
 		echo "Processing clip from ${ts[0]} to ${ts[1]}"
 		if [[ ! "${raw_subs[i]}" == "null" ]]; then
-			gtts-cli -o "$ODIR/tmp/${ts[0]}.trans.m4a" ${raw_subs[i]}
+			gtts-cli -o "$ODIR/tmp/${ts[0]}.trans.m4a" "${raw_subs[i]}" &> /dev/null
 		else
 			# 1000 Hz sine wave
-			ffmpeg -f lavfi -i "sine=frequency=1000:duration=$(echo "${ts[1]}-${ts[0]}+1" | bc)" \
+			ffmpeg -y -f lavfi -i "sine=frequency=1000:duration=$(echo "${ts[1]}-${ts[0]}+1" | bc)" \
 				-ac 2 "$ODIR/tmp/${ts[0]}.trans.m4a" &> /dev/null
 		fi
 	fi
 
 	# Make sure voice generation succeeded
-	if [[ ! -f "$ODIR/tmp/${ts[0]}.trans.m4a" ]]; then
+	if [[ ! -f "$ODIR/tmp/${ts[0]}.trans.m4a" ]] || [[ ! -s "$ODIR/tmp/${ts[0]}.trans.m4a" ]]; then
 		echo "Failed to generate voice clip $ODIR/tmp/${ts[0]}.trans.m4a. Terminating."
 		exit
 	fi
@@ -216,17 +224,19 @@ echo "Generating video clips ..."
 lenvf=$(ffprobe -i "$ODIR/old.mp4" -show_entries format=duration -v quiet -of csv="p=0" | awk '{printf "%.2f\n", $0}')
 for (( i=-1; i<${#vid_times[@]}; i++ )); do
 	# Only regenerate video if we have not already
-	if [[ ! -f "$ODIR/tmp/pm$i.mp4" ]]; then 
+	if [[ ! -f "$ODIR/tmp/pm$i.mp4" ]] || [[ ! -s "$ODIR/tmp/pm$i.mp4" ]]; then 
 		# Special handling for before the first subtitle
 		if (( i == -1 )); then
 			# Split on commas into the array ts
 			IFS="," ts=(${vid_times[0]})
+			IFS=$' \t\n'
 			echo "Processing clip -1 from 0.00 to ${ts[0]}"
 			ffmpeg -y -t ${ts[0]} -i "$ODIR/old.mp4" -c copy "$ODIR/tmp/pm$i.mp4" &> /dev/null
 		elif (( i != ${#vid_times[@]}-1 )); then
 			# Split on commas into the array ts
 			IFS="," ts0=(${vid_times[$i]})
 			IFS="," ts1=(${vid_times[$i+1]})
+			IFS=$' \t\n'
 			# Length of audio stream with leading 0
 			lena=$(ffprobe -i "$ODIR/tmp/${ts0[0]}.trans.m4a" -show_entries format=duration -v quiet -of csv="p=0" | awk '{printf "%.2f\n", $0}')
 			# Length of the video stream with leading 0
@@ -265,11 +275,12 @@ for (( i=-1; i<${#vid_times[@]}; i++ )); do
 		elif (( i == ${#vid_times[@]}-1 )); then
 			# Split on commas into the array ts
 			IFS="," ts0=(${vid_times[$i]})
+			IFS=$' \t\n'
 			# Length of audio stream with leading 0
 			lena=$(ffprobe -i "$ODIR/tmp/${ts0[0]}.trans.m4a" -show_entries format=duration -v quiet -of csv="p=0" | awk '{printf "%.2f\n", $0}')
 			# Length of the video stream with leading 0
 			lenv=$(echo "$lenvf-${ts0[0]}" | bc | awk '{printf "%.2f\n", $0}')
-			echo "Processing clip from ${ts0[0]} to $lenvf"
+			echo "Processing clip $i from ${ts0[0]} to $lenvf"
 			if (( $(echo "$lena >= $lenv" | bc) )); then
 				# Audio longer than video
 				# Crop video
@@ -302,7 +313,7 @@ for (( i=-1; i<${#vid_times[@]}; i++ )); do
 	fi
 
 	# Make sure video generation was successful
-	if [[ ! -f "$ODIR/tmp/pm$i.mp4" ]]; then 
+	if [[ ! -f "$ODIR/tmp/pm$i.mp4" ]] || [[ ! -s "$ODIR/tmp/pm$i.mp4" ]]; then 
 		echo "Failed to generate video clip $ODIR/tmp/pm$i.mp4. Terminating."
 		exit
 	fi
@@ -328,7 +339,7 @@ cmd="$cmd -map '[v]' -map '[a]' '$ODIR/new.mp4'"
 eval "$cmd" &> /dev/null
 
 # Make sure merge was successful
-if [[ ! -f "$ODIR/new.mp4" ]]; then
+if [[ ! -f "$ODIR/new.mp4" ]] || [[ ! -s "$ODIR/new.mp4" ]]; then
 	echo "Failed to generate video. Terminating."
 	exit
 fi
